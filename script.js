c07212bf00aae16aef46a01a86cb05e07197509f
Sergey Ilkov <ilkov1103@gmail.com>
2020-11-10 06:44:36 +0200

    delete script info

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 document.body.innerHTML = '<h1>Hello from JavaScript</h1>';
 console.log('Test !!!!!!!!!!!!');
 
-const test = document.getElementById('test');
-console.log('Hello world');
-console.log(test);
+
