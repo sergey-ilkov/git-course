490d4c1c8b1ede0456049a5ced8c043b9a878593
Sergey Ilkov <ilkov1103@gmail.com>
2020-11-10 06:42:54 +0200

    script.js add info

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 document.body.innerHTML = '<h1>Hello from JavaScript</h1>';
 console.log('Test !!!!!!!!!!!!');
+
+const test = document.getElementById('test');
+console.log('Hello world');
+console.log(test);
